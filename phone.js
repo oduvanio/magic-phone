21923055c959ce2cca8c2cb1a3964d68a5797256
oduvanio <oduvanio@mail.ru>
2016-10-28 00:15:44 +0400

    Update phone.js

--- phone.js
+++ phone.js
@@ -8,7 +8,7 @@ Event.handler('Controller.onshow', function ()
 			if ($(this).data('phone')) return;
 			var number = $(this).text();
 			var len = number.length;
-			$(this).html(number.substr(0, len-2));
+			$(this).html(number.substr(0, len-3) + '<span style="filter:blur(1px); opacity: 0.8;">-</span><span style="filter:blur(3px); opacity: 0.5;">8</span><span style="filter:blur(4px); opacity: 0.3;">8</span>');
 			$(this).data('phone', number);
 			$(this).css('visibility', 'visible');
 			$(this).click(function () {	
@@ -32,3 +32,4 @@ Event.handler('Controller.onshow', function ()
 		});
 	}
 });
+
