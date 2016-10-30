683d993b99c226e85cab726c6bfb8f8b5cd6c671
oduvanio <oduvanio@mail.ru>
2016-10-30 23:07:16 +0400

    Update phone.js

--- phone.js
+++ phone.js
@@ -6,13 +6,25 @@ Event.handler('Controller.onshow', function ()
 	} else {
 		ph.each(function () {
 			if ($(this).data('phone')) return;
+			var conf = Config.get('magic-phone');
 			var number = $(this).text();
 			var len = number.length;
-			$(this).html(number.substr(0, len-3) + '<span style="filter:blur(1px); opacity: 0.8;">-</span><span style="filter:blur(3px); opacity: 0.5;">8</span><span style="filter:blur(4px); opacity: 0.3;">8</span>');
+			var textEnd;
+			switch (conf['design']) {
+				case 'dotted':
+					textEnd = '<span style="cursor:pointer; color:#3d6e8e">...</span>';
+					break;
+				case 'blur':
+					textEnd = '<span style="filter:blur(1px); opacity: 0.8;">-</span><span style="filter:blur(3px); opacity: 0.5;">8</span><span style="filter:blur(4px); opacity: 0.3;">8</span>';
+					break;
+				case 'background':
+					textEnd = '<span style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,1));"></span>';
+					break;
+			}				
+			$(this).html(number.substr(0, len-3) + textEnd);
 			$(this).data('phone', number);
 			$(this).css('visibility', 'visible');
 			$(this).click(function () {	
-				var conf = Config.get('magic-phone');
 				if (conf['yaCounter']) {
 					//В метрике должна быть создана цель с индентификатором phone
 					if (window['yaCounter'+conf['yaCounter']]) {
@@ -32,4 +44,3 @@ Event.handler('Controller.onshow', function ()
 		});
 	}
 });
-
